1956b19685fd10ac66955cdbd869aff57435e73d
Gabriel <63373336+gnesayas@users.noreply.github.com>
2023-10-10 13:15:05 -0500

    Implement mergesort algorithm

--- javascript.js
+++ javascript.js
@@ -27,4 +27,41 @@
 
   console.log(fibs(25));
   console.log(fibsRec(25));
+
+  function mergeSort(arr) {
+    if (arr.length < 2) {
+        return arr;
+    }
+    const sortedLeftHalf = mergeSort(arr.slice(0, arr.length/2));
+    const sortedRightHalf = mergeSort(arr.slice(arr.length/2, arr.length));
+    
+    let i = 0;
+    let j = 0;
+    let k = 0;
+    while (i < sortedLeftHalf.length && j < sortedRightHalf.length) {
+        if (sortedLeftHalf[i] < sortedRightHalf[j]) {
+            arr[k] = sortedLeftHalf[i];
+            i++;
+        } else {
+            arr[k] = sortedRightHalf[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < sortedLeftHalf.length) {
+        arr[k] = sortedLeftHalf[i];
+        i++;
+        k++;
+    }
+    while (j < sortedRightHalf.length) {
+        arr[k] = sortedRightHalf[j];
+        j++;
+        k++;
+    }
+    return arr;
+  }
+
+  const numbers = Array.from({length: 100}, () => Math.floor(Math.random() * 100));
+  console.log(numbers);
+  console.log(mergeSort(numbers));
 })();
